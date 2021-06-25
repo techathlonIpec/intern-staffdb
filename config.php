28e20b15bd9e8ce17206e8d976b2afe2e390ff79
techathlonIpec <techathlon@ipec.org.in>
2021-06-25 18:22:32 +0530

    Fix and Sanitise previous upload
    Oops, Apparently I should not upload my server config? I wonder Why.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "intern-ashu123";
-    $password = "!Ydx3]B~f`T-*Xum";
+    $username = "██████████";
+    $password = "██████████";
     $dbname = "Staff";
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
