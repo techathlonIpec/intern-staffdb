150aa69b56253ff9d0db95f6319d797bbd00defa
techathlonIpec <techathlon@ipec.org.in>
2021-06-25 18:18:11 +0530

    Let's Go Open Source! :)

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    $servername = "localhost";
+    $username = "intern-ashu123";
+    $password = "!Ydx3]B~f`T-*Xum";
+    $dbname = "Staff";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
